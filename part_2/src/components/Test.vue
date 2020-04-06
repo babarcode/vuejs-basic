1e186e64f6f6e1c5158ffba689379f63800b52ea
Muhammad Babar Jihad <mbabarjihad@gmail.com>
2020-04-06 23:07:23 +0700

    init

---
+++ part_2/src/components/Test.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="test">
+        <h3>{{ judul }}</h3>
+        <hr>
+        <ol>
+            <li v-for="item in todos" :key="item.name">
+                {{ item.name }}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        judul: String
+    },
+    data: function () {
+        return {
+            todos: [
+                {name: 'Install Vue-CLI'},
+                {name: 'Initiate new project'},
+                {name: 'Run project'}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
